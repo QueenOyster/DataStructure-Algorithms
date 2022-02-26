d7355086357622bdc37f1cd3aa0c825e67c62e65
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-02-26 20:40:45 +0900

    upstream vs downstream

--- 1 CodingTest/1 Codility/Fish.py
+++ 1 CodingTest/1 Codility/Fish.py
@@ -1,25 +1,34 @@
 # Lesson 7
-# 37%
+# O(N), 100%
+
+# stack이 필요한 이유!
+# 물고기 하나 vs 상류 (X)
+# 하류 vs 상류 (O)
 
 def solution(A, B):
     N = len(A)
-    fight = 0
-    result = 0
+    lives = len(A)
+    fightMode = 0
+    attacker = list()
     for i in range(N):
-        if fight == 0:
+        if fightMode == 0:
             if B[i] == 0:
-                result += 1
-            else: 
-                fight = 1 # battle start
-                result += 1 # only 1 fish will alives
-                power = A[i]
-        else:
-            if B[i] == 0: # battle
-                if power < A[i]:
-                    fight = 0
-                else:
-                    continue
-            else:
-                result += 1
-                power = A[i]
-    return result
+                pass
+            elif B[i] == 1:
+                attacker.append(A[i])
+                fightMode = 1
+
+        elif fightMode == 1:
+            if B[i] == 0:
+                while attacker:
+                    if attacker[-1] < A[i]:
+                        attacker.pop()
+                        lives -= 1
+                    else:
+                        lives -= 1
+                        break
+                if not attacker:
+                    fightMode = 0
+            elif B[i] == 1:
+                attacker.append(A[i])
+    return lives
