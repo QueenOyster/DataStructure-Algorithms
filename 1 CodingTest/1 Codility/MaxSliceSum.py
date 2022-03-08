ff0720c423109f64c65485350999fa8cc26c0424
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-03-08 23:43:58 +0900

    O(N), number of cases: negative/0/positive

--- 1 CodingTest/1 Codility/MaxSliceSum.py
+++ 1 CodingTest/1 Codility/MaxSliceSum.py
@@ -1,1 +1,19 @@
 # Lesson 9
+# O(N)
+
+def solution(A):
+  
+    max_ending = max_slice = 0
+
+    largest_non_positive = -2147483648
+    for a in A:
+        max_ending = max(0, max_ending + a)
+        max_slice = max(max_slice, max_ending)
+        if a <= 0:
+            largest_non_positive = max(largest_non_positive, a)
+
+    
+    if max_slice > 0:
+        return max_slice
+    elif max_slice == 0:
+        return largest_non_positive
