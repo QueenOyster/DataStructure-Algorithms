40986c8ef1c3c2dbdecffd516ac5fcce4538e76c
DESKTOP-17R30V6\david <nekoder777@gmail.com>
2022-03-12 02:29:25 +0900

    Counting Factors(Divisors)

--- 1 CodingTest/1 Codility/CountFactors.py
+++ 1 CodingTest/1 Codility/CountFactors.py
@@ -1,22 +1,16 @@
+# Lesson 10, Prime and composite numbers
+# O(sqrt(N))
+
 def solution(N):
-    checker = [0] * (1 + N//2)
-    checker[0] = 1
-    checker[1] = 1
+  
+    i = 1
+    result = 0
+    while i * i < N:
+        if N % i == 0:
+            result += 2
+        i += 1
 
-    for i in range(2, N//2 + 1):
-        k = 0
-        while True:
-            k += i
-            if checker[k] == 0:
-                checker[k] = 1
-            if k + i <= N//2:
-                continue
-            else:
-                break
-    
-    cnt = 0
-    for element in checker:
-        if element == 0:
-            cnt += 1
-
-    return cnt
+    if i * i == N:
+        result += 1
+        
+    return result
